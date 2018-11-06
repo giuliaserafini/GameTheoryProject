24be20bd44af53c84dd35990d621db8bd082391f
Alessandro Toschi <alessandro@MacBook-Pro-di-Alessandro.local>
2018-11-06 21:59:13 +0800

    Added some comments

--- gateway_selection.py
+++ gateway_selection.py
@@ -13,50 +13,69 @@ class Gateway(object):
     """
     This class describes an IOT gateway, with its bandwidth, shared among all the devices, and the reachability radius.
     """
-    COUNTER = 0
+    COUNTER = 0 # Autoincrement ID.
 
     def __init__(self, bandwidth, radius=25):
-        if bandwidth is None or bandwidth <= 0:
+        """
+        Initialize a device given a bandwidth [Mbps] and an optional radius [m].\n
+        If the radius is omitted, it is assumed to be equal to 25 m.
+        """
+        if bandwidth is None or bandwidth <= 0: # The bandwidth must be an integer greater than zero.
             raise ValueError("Bandwidth must be greater than zero.")
         
-        if radius is None or radius <= 0:
+        if radius is None or radius <= 0: # The radius must be an integer greater than zero. 
             raise ValueError("Radius must be greater than zero.")
         
-        Gateway.COUNTER += 1
+        Gateway.COUNTER += 1 # Increment the ID.
 
+        # Assign the parameters or initialize to default values.
         self.__ID = Gateway.COUNTER
         self.__bandwidth = bandwidth
         self.__radius = radius
         self.__devices = []
 
     def connect_device(self, device):
-        if device is None or not isinstance(device, Device):
+        """
+        Connect a device to the gateway, if not already connected.
+        """
+        if device is None or not isinstance(device, Device): # The device's class must be Device and it has to be not null.
             raise ValueError("The device must be not null.")
-        if device not in self.__devices:
-            self.__devices.append(device)
+        if device not in self.__devices: # The device must not be already connected to the device.
+            self.__devices.append(device) # Insert the device.
     
     def disconnect_device(self, device):
-        if device is None or not isinstance(device, Device):
+        """
+        Disconnect a device from the gateway, if it was connected.
+        """
+        if device is None or not isinstance(device, Device): # The device's class must be Device and it has to be not null.
             raise ValueError("The device must be not null.")
-        if device in self.__devices:
-            self.__devices.remove(device)
+        if device in self.__devices:# The device must be already connected to the device.
+            self.__devices.remove(device) # Delete the device.
     
     def get_device_count(self, type):
+        """
+        Return the number of devices connected to the gateway of the speciefied type.
+        """
+        # Return the number of devices that their type is equal to the given one.
         return len([device for device in self.__devices if device.type == type])
 
     def __eq__(self, other):
+        # Compare the identifier of two instances.
         return other.ID == self.ID if isinstance(other, Gateway) else False
     
     def __hash__(self):
+        # Compute the hash using the ID, bandwidth and radius.
         return hash((self.__ID, self.__bandwidth, self.__radius))
     
     def __repr__(self):
+        # The string is 'Gateway15' for a gateway, having 15 as ID.
         return "Gateway" + str(self.__ID)
     
     def __str__(self):
         return self.__repr__()
     
     def __contains__(self, item):
+        # Check if a device is connected to the gateway.
         return item in self.__devices
     
     ID = property(lambda self: self.__ID)
@@ -65,12 +84,21 @@ class Gateway(object):
     radius = property(lambda self: self.__radius)
 
 class Displacement(object):
+    """
+    This class represents a displacement of a device or gateway in a two-dimensional space.\n
+    This object is immutable.
+    """
     def __init__(self, x, y, element):
+        """
+        Initialize the object given a position (x, y) and and element.
+        """
+        # Set the given parameters.
         self.__x = x
         self.__y = y
         self.__element = element
     
     def __eq__(self, other):
+        # Compare two instances according the position and the element.
         return  other.x == self.x and \
                 other.y == self.y and \
                 other.element == self.element if isinstance(other, Displacement) \
@@ -81,48 +109,71 @@ class Displacement(object):
     element = property(lambda self: self.__element)
 
 class Space(object):
+    """
+    This class models a two-dimensional space where is possible to place devices and gateways.
+    """
     def __init__(self, rows, columns):
-        if rows is None or rows <= 0:
+        """
+        Initialize the object given the dimensions: rows and columns.
+        Both dimensions must be integers, grater than zero.
+        """
+        if rows is None or rows <= 0: # The rows must an integer greater than zero.
             raise ValueError("Rows must be greater than zero.")
-        if columns is None or columns <= 0:
+        if columns is None or columns <= 0: # The columns must an integer greater than zero.
             raise ValueError("Columns must be greater than zero.")
 
+        # Assign the parameters and the default values.
         self.__rows = rows
         self.__columns = columns
         self.__positions = []
     
     def add_element(self, element, x=None, y=None):
+        """
+        Add an element to the space.\n
+        If the position (x, y) is omitted, a random one is generated.\n
+        If the element is already placed an exception will be raised.\n
+        If the position (x, y) is provided, it must be within the boundaries.\n
+        Each device is placed in a position, such that it can connect to at least one gateway.
+        """
+        # If the space already contains a position with that element, raise an exception.
         if len([displacement for displacement in self.__positions if displacement.element == element]) > 0:
             raise RuntimeError("The element is already placed.")
+        # If the position provided is out of boundaries, raise an exception.
         if (x is not None and (x < 0 or x >= self.__columns)) or (y is not None and (y < 0 or y >= self.__rows)):
             raise IndexError("The position (x, y) must be within the boundaries.")
         if x is not None and y is not None:
+            # Create a new displacement.
             new_displacement = Displacement(x, y, element)
+            # If the position is already taken, raise an exception.
             if len([displacement for displacement in self.__positions if displacement.x == x and displacement.y == y]) > 0:
                 raise ValueError("The position ({},{}) is already taken.".format(x, y))
-            self.__positions.append(new_displacement)
-            if isinstance(element, Device):
-                near_gateways = self.get_near_gateways(element)
-                if len(near_gateways) == 0:
+            self.__positions.append(new_displacement) # If all of the previous constrains hold, place the element.
+            if isinstance(element, Device): # If the element is a device, than it should be near at least one gateway.
+                near_gateways = self.get_near_gateways(element) # Find the nearest gateways of a device.
+                if len(near_gateways) == 0: # If it's isolated, remove the displacement inserted before and raise an exception.
                     self.__positions.remove(new_displacement)
                     raise ValueError("The device is not able to connect to any gateway.")
                 else:
+                    # If there is at least one near gateway, select one from the neighbors and setup the connection.
                     random_gateway_index = randint(0, len(near_gateways) - 1)
                     near_gateways[random_gateway_index].connect_device(element)
-            return (x, y)
+            return (x, y) # Return the position of the element.
         else:
-            added = False
+            added = False # The element is not initially added.
+            # Initialize the position.
             x = 0
             y = 0
-            while not added:
+            while not added: # While the position was not suitable for the element:
+                # Generate a random position.
                 x = randint(0, self.__columns - 1)
                 y = randint(0, self.__rows - 1)
                 try:
+                    # Try to add the element.
                     (x, y) = self.add_element(element, x=x, y=y)
-                    added = True
-                except ValueError as error:
+                    added = True # Everything gone fine, can exit.
+                except ValueError as error: # If the exception was a value error than we have to retry, until we find a valid position.
                     continue
-            return (x, y)
+            return (x, y) # Return the position.
 
     def get_near_gateways(self, element):
         if element is None or len([displacement for displacement in self.__positions if displacement.element == element]) == 0:
